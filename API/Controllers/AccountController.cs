bf94b5c2823b541b64c1abaa9f327e824cbc9ca1
faizu2k01 <faizhaque35@gmail.com>
2023-06-26 10:58:40 +0530

    initial -1.0

---
+++ API/Controllers/AccountController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace APi.Controllers
+{
+    public class AccountController : BaseController
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
